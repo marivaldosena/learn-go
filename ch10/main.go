06aa153667061a0b9baf4df612366f3e7bfa511e
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-10 13:47:32 -0300

    Incluir aula sobre concorrência e Channels

---
+++ ch10/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func f(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n, ":", i)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
+	}
+}
+
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+
+func ponger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
+}
+
+func printer(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println("Type Enter to exit...", msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go f(i)
+	}
+	var input string
+	var c chan string = make(chan string)
+
+	go pinger(c)
+	go ponger(c)
+	go printer(c)
+
+	fmt.Scanln(&input)
+}
