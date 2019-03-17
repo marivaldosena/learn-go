6fdd15eddab25ce95d2e3c9236c983ca34e0046a
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-17 19:49:19 -0300

    Incluir exemplo de ordenação de structs

--- ch14/main.go
+++ ch14/main.go
@@ -3,9 +3,29 @@ package main
 import (
 	"container/list"
 	"fmt"
+	"sort"
 )
 
-func main() {
+type Person struct {
+	Name string
+	Age  int
+}
+
+type ByName []Person
+
+func (this ByName) Len() int {
+	return len(this)
+}
+
+func (this ByName) Less(i, j int) bool {
+	return this[i].Name < this[j].Name
+}
+
+func (this ByName) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+}
+
+func usingContainerList() {
 	var x list.List
 
 	x.PushBack(1)
@@ -15,4 +35,22 @@ func main() {
 	for e := x.Front(); e != nil; e = e.Next() {
 		fmt.Println(e.Value.(int))
 	}
+}
+
+func orderList(people []Person) {
+	sort.Sort(ByName(people))
+}
+
+func main() {
+	usingContainerList()
+
+	kids := []Person{
+		{"Jill", 9},
+		{"Jack", 10},
+		{"Joe", 8},
+		{"Julie", 7},
+	}
+
+	orderList(kids)
+	fmt.Println(kids)
 }
